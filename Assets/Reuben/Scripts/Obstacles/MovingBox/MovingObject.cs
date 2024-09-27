8047c583323a82b437572c7c74e428feb8cbe8e2
reuben4046 <poolereuben@gmail.com>
2024-09-27 15:02:19 +1000

    moving object prefab updates

--- Assets/Reuben/Scripts/Obstacles/MovingBox/MovingObject.cs
+++ Assets/Reuben/Scripts/Obstacles/MovingBox/MovingObject.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine.EventSystems;
 
 public class MovingObject : MonoBehaviour
 {
     [Title ("Settings")]
-    public Direction direction;
-    public enum Direction
+    public MoveAxis axis;
+    public enum MoveAxis
     {
         X,
         Y,
         Z
     }
+    
+    public MoveDirection direction;
+    public enum MoveDirection
+    {
+        Positive,
+        Negative
+    }
 
-    private Vector3 moveDirection;
+    private Vector3 moveAxis;
     public float moveAmount;
     public float moveSpeed;
 
     Vector3 startPosition;
 
+    void OnValidate()
+    {
+        startPosition = transform.position;
+    }
 
     void Start()
     {
@@ -32,22 +44,49 @@ public class MovingObject : MonoBehaviour
         DirectionSwitcher();
         
         Vector3 v = startPosition;
-        v += moveAmount * Mathf.Sin(Time.time * moveSpeed) * moveDirection;
+        v += moveAmount * Mathf.Sin(Time.time * moveSpeed) * moveAxis;
         transform.position = v;
     }
 
     void DirectionSwitcher()
     {
+        switch (axis)
+        {
+            case MoveAxis.X:
+                    moveAxis = new Vector3(1, 0, 0);
+                break;
+            case MoveAxis.Y:
+                    moveAxis = new Vector3(0, 1, 0);
+                break;
+            case MoveAxis.Z:
+                moveAxis = new Vector3(0, 0, 1);
+                break;
+        }
         switch (direction)
         {
-            case Direction.X:
-                    moveDirection = new Vector3(1, 0, 0);
+            case MoveDirection.Positive:
+                moveAxis *= 1;
                 break;
-            case Direction.Y:
-                    moveDirection = new Vector3(0, 1, 0);
+            case MoveDirection.Negative:
+                moveAxis *= -1;
                 break;
-            case Direction.Z:
-                moveDirection = new Vector3(0, 0, 1);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        
+        switch (axis)
+        {
+            case MoveAxis.X:
+                Gizmos.DrawLine(startPosition + new Vector3(moveAmount, 0, 0), startPosition + new Vector3(-moveAmount, 0, 0));
+                break;
+            case MoveAxis.Y:
+                Gizmos.DrawLine(startPosition + new Vector3(0, moveAmount, 0), startPosition + new Vector3(0, -moveAmount, 0));
+                break;
+            case MoveAxis.Z:
+                Gizmos.DrawLine(startPosition + new Vector3(0, 0, moveAmount), startPosition + new Vector3(0, 0, -moveAmount));
                 break;
         }
     }
