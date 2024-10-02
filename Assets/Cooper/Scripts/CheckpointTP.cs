a628ee4c3696326eb201d49c572ae6723eef9aab
Cooper-Rampling <134895307+Cooper-Rampling@users.noreply.github.com>
2024-10-02 14:15:26 +1000

    Checkpoint_Sysytem
    Added checkpoints into scene

---
+++ Assets/Cooper/Scripts/CheckpointTP.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CheckpointTP : MonoBehaviour
+{
+    [SerializeField] private Vector3 currentCheckpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.position = currentCheckpoint;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CheckPoint") 
+        { 
+            currentCheckpoint = other.transform.position;
+        }
+        currentCheckpoint.y += 1;
+        other.tag = "UsedPoint";
+    }
+}
